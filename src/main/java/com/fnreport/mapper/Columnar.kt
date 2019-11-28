150f9ff1810cf188e3db4331d8d05e646cbf827b
Jim Northrup <jim@vsiwest.com>
2019-11-28 22:05:29 +0700

    group composability proven in columnarTest.kt
    tests check values now

--- src/main/java/com/fnreport/mapper/Columnar.kt
+++ src/main/java/com/fnreport/mapper/Columnar.kt
@@ -10,8 +10,6 @@ import java.nio.channels.FileChannel
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
-import kotlin.math.max
-import kotlin.math.min
 
 val Pair<Int, Int>.size: Int get() = let { (a, b) -> b - a }
 typealias ByteBufferNormalizer<T> = Pair<Pair<Int, Int>, (Any?) -> T>
@@ -92,21 +90,6 @@ interface RowStore<T> : Flow<T> {
     @InternalCoroutinesApi
     override suspend fun collect(collector: FlowCollector<T>) {
           (0 until size).forEach{  collector.emit(values(it)) }
-
-/*
-        val availableProcessors = Runtime.getRuntime().availableProcessors()
-        val chunksize = max(size, availableProcessors) / min(size, availableProcessors)
-*/
-        /*
-        coroutineScope {
-            (0 until size).chunked(chunksize).forEach { range ->
-                launch {
-                    coroutineScope {
-                        range.forEach { launch { collector.emit(values(it)) } }
-                    }
-                }
-            }
-        }*/
     }
 }
 
@@ -231,165 +214,52 @@ private fun pivotData(data: Flow<Array<Any?>>, lhs: IntArray, xcoord: Map<Any?, 
         }
     }
 }
-/*
 
-*/
+inline operator fun <reified T> Array<T>.get(vararg index: Int) = index.map(::get).toTypedArray()
+
+
 /**
- * our tight recursive object with flexible column features
- *//*
+ * cost of one full tablscan
+ */
+suspend fun Table2.group(vararg by: Int): Table2 = let {
+    val (columns, data) = this
+    val (rows, d) = data
+    val protoValues = (columns.indices - by.toTypedArray()).toIntArray()
+    val clusters = mutableMapOf<Int,Pair<  Array<*>,MutableList<Flow<Array<Any?>>>>>()
+    rows.collect { row ->
+        val key = row.get(*by)
+        val keyHash = key.contentDeepHashCode()
+        flowOf(row.get(*protoValues)).let { f ->
+            if (clusters.containsKey(keyHash)) clusters[keyHash]!!.second  += (  f)
+            else clusters[keyHash] = key to mutableListOf( f)
+        }
+    }
+    columns to (clusters.map { (k, cluster1) ->
+        val (key,cluster)=cluster1
+       assert(key.size == by.size)
+        arrayOfNulls<Any?>(columns.size).also { finale ->
 
-@InternalCoroutinesApi
-open class Columnar(var rs: RowStore<Flow<ByteBuffer>>, val columns: Array<Pair<String, Pair<Pair<Int, Int>, (Any?) -> Any?>>>) : RowStore<List<*>> {
+            by.mapIndexed { index, i ->
+                finale[i] = key[index]
+            }
+            val groupedRow = protoValues.map { arrayListOf<Any?>() }.let { cols ->
 
-    override val size: Int = rs.size
-    operator fun get(vararg cols: Int): Columnar = this[cols.toList()]
-    operator fun get(cols: List<Int>) = Columnar(this.rs, cols.map { columns[it] }.toTypedArray())
-    operator fun get(xform: Pair<Iterable<Int>, (Any?) -> Any?>): Unit {
-        xform.let { (indices, transform) ->
-            indices.map { indice ->
-                columns[indice].let { (a, b) ->
-                    b.let { (c, rowMapper) ->
-                        columns[indice] = a to (c to
-                                { any -> transform(rowMapper(any)) })
+                cluster.forEach { group ->
+                    group.toList().forEachIndexed { index: Int, row: Array<Any?> ->
+                        assert(row.size == protoValues.size)
+                        row.forEachIndexed { index, any -> cols[index] += any }
                     }
+                }
+                assert(cols.size == protoValues.size)
+                cols.map {
+                    assert(it.size == cluster.size)
+                    it.toTypedArray()
                 }
             }
-        }
-    }
-
-
-    override var values: suspend (Int) -> List<*> = { row: Int ->
-        rs.values(row).first().let { rs1 ->
-            (columns.map { (a, mapper) ->
-                val (coor, conv) = mapper
-                val (begin, end) = coor
-                val len = end - begin
-                val fb = rs1.position(begin).slice().limit(len)
-                conv(ByteArray(len).also { fb.get(it) })
-            })
-        }
-    }
-
-
-    suspend fun pivot(untouched: IntArray, lhs: Int, vararg rhs: Int): Columnar {
-        val sets1 = linkedMapOf<Any?, SortedSet<Int>>()
-
-        run {
-            this[lhs].run {
-                this.collectIndexed { index, row ->
-                    val key = row.first()
-                    val cluster = sets1[key]
-                    when (cluster) {
-                        null -> sets1[key] = (sortedSetOf(index))
-                        else -> cluster.add(index)
-                    }
-                }
+            protoValues.mapIndexed { index, i ->
+                finale[i] = groupedRow[index]
             }
+
         }
-
-        val sets = sets1.map { (k, v) -> k to (v to IntArray(1)) }.toMap()
-        var c = 0
-        untouched.map { columns[it] }.toMutableList().let { revisedColumns ->
-            this.columns[lhs].let { (keyPrefix) ->
-                sets.entries.map { (k, v) ->
-                    v.second[0] = c
-                    c++
-                    "$keyPrefix:${(k as? ByteArray)?.let { it -> String(it) } ?: k}".let { keyName ->
-                        revisedColumns += rhs.map { rhsCol ->
-
-                            this.columns[rhsCol].let { (rhsName, decode) ->
-                                val (coords, mapper) = decode
-
-
-                                ("$keyName,$rhsName") to (coords to mapper)
-                            }
-                        }
-                    }
-                }
-                val origin = this
-                val clusters = sets.map { (k, v) -> k.hashCode() to v.let { (a, b) -> a.toIntArray() to b.first() } }.toMap()
-                return object : Columnar(rs, revisedColumns.toTypedArray()) {
-                    override var values: suspend (Int) -> List<*> = { row ->
-                        //  rs.values(row).let { rs1 ->
-                        //      super.columns.mapIndexed { ix, (_, mapper) ->
-                        //          val (coor, conv: (Any?) -> Any?) = mapper
-                        //          val (begin, end) = coor
-                        //          val len = end - begin
-                        //          val fb = rs1.first().position(begin).slice().limit(len)
-                        //          if (ix < untouched.size) conv(ByteArray(len).also { fb.get(it) })
-                        //          else conv(row to ByteArray(len).also { fb.get(it) })
-                        //      }
-                        //  }
-                        val values1 = origin.values(row)
-
-                        val front = untouched.size
-                        lateinit var cluster: Pair<IntArray, Int>
-
-                        val second = cluster.second
-                        val lower = second * rhs.size
-                        val intRange = lower until (lower + rhs.size)
-                        coroutineScope {
-                            sequence {
-                                columns.indices.forEach { colNum ->
-
-                                    when {
-                                        colNum < front -> yield(values1[untouched[colNum]])
-                                        colNum == front -> {
-                                            val any = values1[lhs]
-                                            val hashCode = any.hashCode()
-                                            val pair = clusters[hashCode]
-                                            cluster = pair!!
-                                        }
-                                        colNum > front -> {
-                                            val adjusted = colNum - front
-                                            yield(if (adjusted in intRange) {
-                                                values1[rhs[(adjusted - intRange.first)]]
-                                            } else null)
-                                        }
-                                    }
-                                }
-                            }.toList()
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    @InternalCoroutinesApi
-    class GroupColumnar(private val origin: Columnar, override val size: Int, private val originClusters: Array<IntArray>,
-                        private val gby: IntArray) : Columnar(origin.rs, origin.columns) {
-        override var values: suspend (Int) -> List<*> = { row ->
-            originClusters[row].let { cluster ->
-                cluster.first().let { keyRowNum ->
-                    (origin.values(keyRowNum) to (cluster.map { gRow ->
-                        origin.values(gRow)
-                    }).map { it }).let { (keyRow, aggvals) ->
-                        columns.indices.map { colNum ->
-                            if (colNum in gby)
-                                keyRow[colNum]
-                            else
-                                aggvals.map { it[colNum] }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    @InternalCoroutinesApi
-    suspend fun group(vararg by: Int) = sortedMapOf<Int, MutableSet<Int>>().let { collate ->
-        this.get(*by).collectIndexed { index, list ->
-            val hashCode = list.hashCode()
-            val v = collate[hashCode]
-            if (v != null) v.add(index) else collate[hashCode] = sortedSetOf(index)
-        }
-        GroupColumnar(this, collate.size, collate.values.map { it.toIntArray() }.toTypedArray(), by)
-    }
+    }.asFlow() to clusters.size)
 }
-*/
-
-//todo: csv analogs.  for the same file we would have recordlen as Array<Int>, and our columns
-// would have to parse and escape tokens for each cell value.  the group/pivot logic would
-// be affected by column structure changes.
-
--- src/test/java/com/fnreport/mapper/ColumnarTest.kt
+++ src/test/java/com/fnreport/mapper/ColumnarTest.kt
@@ -24,7 +24,6 @@ class ColumnarTest : StringSpec() {
 
     val c20 = columns from f20
     val c4 = columns from f4
-    val c4remap = c4[0, 0, 0, 1, 3, 2, 1, 1, 1]
 
 
     override fun beforeTest(testCase: TestCase) {
@@ -43,8 +42,13 @@ class ColumnarTest : StringSpec() {
         "remap"{
             val c41 = c4(1).map { it.first() }
             System.err.println(c41)
+            c41.last().shouldBe(0.0)
+
+            val c4remap = c4[0, 0, 0, 1, 3, 2, 1, 1, 1]
+
             val map = c4remap(1).map { it.first() }
             System.err.println(map)
+            map.last().shouldBe(map[3] )
         }
         "reify"{
             val suspendFunction1 = columns from f4
@@ -56,20 +60,14 @@ class ColumnarTest : StringSpec() {
                 val (c, d) = b
                 c.collect {
                     System.err.println(it.asList())
-
                 }
-                c.collect {
-                    System.err.println(it.asList())
-                }
+                c.count().shouldBe(4)
             }
             x()
-
         }
-
         "pivot" {
             System.err.println("pivot")
             val x = suspend {
-
                 val reify = columns reify f4
                 val p4 = reify.pivot(intArrayOf(0), intArrayOf(1), 2, 3)
                 p4.let { (col, data) ->
@@ -78,6 +76,9 @@ class ColumnarTest : StringSpec() {
                         rows.collect { arr ->
                             System.err.println(arr.toList())
                         }
+                        rows.first().last().shouldBe(null)
+                        val last = rows. toList().last().dropLast(1).last().shouldBe(820.0)
+
                     }
                 }
             }
@@ -86,109 +87,45 @@ class ColumnarTest : StringSpec() {
 
         "group" {
 
-            val f20 = FixedRecordLengthFile("src/test/resources/caven20.fwf")
-            val f4 = FixedRecordLengthFile("src/test/resources/caven4.fwf")
-
-            val c20 = columns from f20
-            val c4 = columns from f4
             System.err.println("group")
             val x = suspend {
-
                 val r4 = columns reify f4
-                val clusters = r4.clusters(0)
-
+                var clusters = r4.group(1)
                 clusters.let { (a, b) ->
                     val (c, d) = b
+                    System.err.println("by col 1")
                     System.err.println("${a.asList()} ")
                     c.collect {
-
                         System.err.println(it.contentDeepToString())
                     }
+                    c.count().shouldBe(3)
                 }
-/*
-                val index = r4.index(0)
-                System.err.println("${index.asList()}")
-*/
-//
-//                val clusters1 = r4.clusters(1)
-//                System.err.println("$clusters1")
+                clusters = r4.group(0)
+                clusters.let { (a, b) ->
+                    val (c, d) = b
+                    System.err.println("by col 0")
+                    System.err.println("${a.asList()} ")
+                    c.collect {
+                        System.err.println(it.contentDeepToString())
+                    }
+                    c.count().shouldBe(3)
+                }
+                clusters = clusters.group(3)
+                clusters.let { (a, b) ->
+                    val (c, d) = b
+                    System.err.println("composability by previous group to column 3  ")
+                    System.err.println("${a.asList()} ")
+                    c.collect {
+                        System.err.println(it.contentDeepToString())
 
-
+                    }
+                    c.count().shouldBe(2)
+                }
             }
             x()
         }
-
-    }
-}
-/*
-
-suspend fun Table2.index(vararg by: Int) = clusters(*by).let { clust ->
-    this.let { (colnames, data) ->
-        data.let { (rows, size) ->
-
-            */
-/**
- * use elevator to stuff flows into lists.
- *//*
-
-            val elevator = IntArray(size)
-
-            clust.values.mapIndexed { index, list ->
-
-                rows.transform {
-
-
-                }
-            }
-        }
-    }
-}
-*/
-
-inline operator fun <reified T> Array<T>.get(vararg index: Int) = index.map(::get).toTypedArray()
-
-
-/**
- * cost of one full tablscan
- */
-suspend fun Table2.clusters(vararg by: Int): Table2 = let {
-    val (columns, data) = this
-    val (rows, d) = data
-    val protoValues = (columns.indices - by.toTypedArray()).toIntArray()
-    val clusters = mutableMapOf<List<Any?>, MutableList<Flow<Array<Any?>>>>()
-    rows.collect { row ->
-        val key = by.map { row[it] }
-        flowOf(row.get(*protoValues)).let { f ->
-            if (clusters.containsKey(key)) clusters[key]!! += (f)
-            else clusters[key] = mutableListOf(f)
-        }
-    }
-    columns to (clusters.map { (k, cluster) ->
-        assert(k.size == by.size)
-        arrayOfNulls<Any?>(columns.size).also { finale ->
-            by.mapIndexed { index, i ->
-                finale[i] = k[index]
-            }
-            val groupedRow = protoValues.map { arrayListOf<Any?>() }.let { cols ->
-
-                cluster.forEach { group ->
-                    val cList = group.toList()
-//                    assert(cList.size == cluster.size)
-                    cList.forEachIndexed { index: Int, row: Array<Any?> ->
-                        assert(row.size == protoValues.size)
-                        row.forEachIndexed { index, any -> cols[index] += any }
-                    }
-                }
-                assert(cols.size == protoValues.size)
-                cols.map {
-                    assert(it.size == cluster.size)
-                    it.toTypedArray()
-                }
-            }
-            protoValues.mapIndexed { index, i ->
-                finale[i] = groupedRow[index]
-            }
+        "composability" {
 
         }
-    }.asFlow() to clusters.size)
+    }
 }
