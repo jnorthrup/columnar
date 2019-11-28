1e81b37d5f07e5dc703b32ac8076960986be2524
Jim Northrup <jim@vsiwest.com>
2019-11-29 00:12:54 +0700

    solidify type=info and paramters for general onlookers.

--- src/test/java/com/fnreport/mapper/ColumnarTest.kt
+++ src/test/java/com/fnreport/mapper/ColumnarTest.kt
@@ -3,6 +3,7 @@ package com.fnreport.mapper
 import io.kotlintest.TestCase
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import java.util.*
@@ -10,6 +11,7 @@ import java.util.*
 fun Array<*>.equalsArray(other: Array<*>) = Arrays.equals(this, other)
 fun Array<*>.deepEqualsArray(other: Array<*>) = Arrays.deepEquals(this, other)
 
+@ExperimentalCoroutinesApi
 @UseExperimental(InternalCoroutinesApi::class)
 class ColumnarTest : StringSpec() {
     val columns: List<Pair<String, Pair<Pair<Int, Int>, (Any?) -> Any?>>> = listOf("date", "channel", "delivered", "ret").zip(
@@ -22,8 +24,8 @@ class ColumnarTest : StringSpec() {
     val f20 = FixedRecordLengthFile("src/test/resources/caven20.fwf")
     val f4 = FixedRecordLengthFile("src/test/resources/caven4.fwf")
 
-    val c20 = columns from f20
-    val c4 = columns from f4
+    val c20: Table1 = columns from f20
+    val c4: Table1 = columns from f4
 
 
     override fun beforeTest(testCase: TestCase) {
@@ -32,52 +34,55 @@ class ColumnarTest : StringSpec() {
     init {
         "dateCol"{
             val any = c20(1)[0].first()
-            any.toString().shouldBe("2017-10-22")
-            System.err.println(any)
+            any.toString() shouldBe "2017-10-22"
+            println(any)
         }
         "size" {
-            f4.size.shouldBe(4)
+            f4.size shouldBe 4
 
         }
         "remap"{
-            val c41 = c4(1).map { it.first() }
-            System.err.println(c41)
-            c41.last().shouldBe(0.0)
+            val c41: List<Any?> = c4(1).map { it.first() }
+            println(c41)
+            c41.last() shouldBe 0.0
 
-            val c4remap = c4[0, 0, 0, 1, 3, 2, 1, 1, 1]
+            val c4remap: Table1 /* suspend (Int) -> Array<Flow<Any?>>*/ = c4[0, 0, 0, 1, 3, 2, 1, 1, 1]
+            val firstrow: Array<Flow<Any?>> = c4remap(0)
+            val reorderedRow: List<Any?> = firstrow.map { flow ->
+                flow.first()
+            }
+            println(reorderedRow)
+            reorderedRow.last() shouldBe reorderedRow[3]
 
-            val map = c4remap(1).map { it.first() }
-            System.err.println(map)
-            map.last().shouldBe(map[3] )
+
         }
         "reify"{
-            val suspendFunction1 = columns from f4
-            val stage = f4.take(f4.size)
-            val r4 = columns reify f4
+
+            val r4: Table2 = columns reify f4
             val x = suspend {
-                System.err.println("reify")
-                val (a, b) = r4
-                val (c, d) = b
-                c.collect {
-                    System.err.println(it.asList())
+                println("reify")
+                val (_: Array<String>, data: Pair<Flow<Array<Any?>>, Int>) = r4
+                val (rows, _) = data
+                rows.collect<Array<Any?>> {
+                    println(it.asList())
                 }
-                c.count().shouldBe(4)
+                rows.count() shouldBe 4
             }
             x()
         }
         "pivot" {
-            System.err.println("pivot")
+            println("pivot")
             val x = suspend {
-                val reify = columns reify f4
-                val p4 = reify.pivot(intArrayOf(0), intArrayOf(1), 2, 3)
-                p4.let { (col, data) ->
-                    System.err.println(col.map { it })
-                    data.let { (rows, size) ->
-                        rows.collect { arr ->
-                            System.err.println(arr.toList())
+                val r4: Table2 = columns reify f4
+                val p4: Table2 = (r4).pivot(/*lhs=*/intArrayOf(0), /*axis =*/ intArrayOf(1), /*...fanout=*/2, 3)
+                p4.let { (col: Array<String>, data: Pair<Flow<Array<Any?>>, Int>) ->
+                    println(col.map { it })
+                    data.let { (rows: Flow<Array<Any?>> ) ->
+                        rows.collect { arr: Array<Any?> ->
+                            println(arr.toList())
                         }
-                        rows.first().last().shouldBe(null)
-                        val last = rows. toList().last().dropLast(1).last().shouldBe(820.0)
+                        rows.first().last() shouldBe null
+                        rows.toList().last().dropLast(1).last() shouldBe 820.0
 
                     }
                 }
@@ -87,45 +92,57 @@ class ColumnarTest : StringSpec() {
 
         "group" {
 
-            System.err.println("group")
+            println("group")
             val x = suspend {
-                val r4 = columns reify f4
-                var clusters = r4.group(1)
-                clusters.let { (a, b) ->
-                    val (c, d) = b
-                    System.err.println("by col 1")
-                    System.err.println("${a.asList()} ")
-                    c.collect {
-                        System.err.println(it.contentDeepToString())
+                val r4: Table2 = columns reify f4
+                var clusters: Table2 = r4.group(1)
+                clusters.let { (colnames: Array<String>, data: Pair<Flow<Array<Any?>>, Int>) ->
+                    val (rows ) = data
+                    println("by col 1")
+                    println("${colnames.asList()} ")
+                    rows.collect { arrayOfAnys: Array<Any?> ->
+                        println(arrayOfAnys.contentDeepToString())
                     }
-                    c.count().shouldBe(3)
+                    rows.count() shouldBe 3
                 }
-                clusters = r4.group(0)
-                clusters.let { (a, b) ->
-                    val (c, d) = b
-                    System.err.println("by col 0")
-                    System.err.println("${a.asList()} ")
-                    c.collect {
-                        System.err.println(it.contentDeepToString())
+                clusters = r4.group( /*...by=*/0)
+                clusters.let { (cnames: Array<String>, data: Pair<Flow<Array<Any?>>, Int>) ->
+                    val (rows, _) = data
+                    println("by col 0")
+                    println("${cnames.asList()} ")
+                    rows.collect { arrayOfAnys: Array<Any?> ->
+                        println(arrayOfAnys.contentDeepToString())
                     }
-                    c.count().shouldBe(3)
+                    rows.count() shouldBe 3
                 }
                 clusters = clusters.group(3)
-                clusters.let { (a, b) ->
-                    val (c, d) = b
-                    System.err.println("composability by previous group to column 3  ")
-                    System.err.println("${a.asList()} ")
-                    c.collect {
-                        System.err.println(it.contentDeepToString())
+                clusters.let { (names, data) ->
+                    val (rows: Flow<Array<Any?>>, _: Int) = data
+                    println("composability by previous group to column 3  ")
+                    println("${names.asList()} ")
+                    rows.collect { arrayOfAnys: Array<Any?> ->
+                        println(arrayOfAnys.contentDeepToString())
 
                     }
-                    c.count().shouldBe(2)
+                    rows.count() shouldBe 2
                 }
             }
             x()
         }
         "composability" {
+            println("pivotpivot")
+            val x = suspend {
+                var p4 = (columns reify f4).pivot(/*lhs = */intArrayOf(0),/* axis = */intArrayOf(1),/*fanout...*/ 2, 3)
+                println(p4.first.contentDeepToString())
+                p4.second.first.collect { println(it.contentDeepToString()) }
 
+                val (compoundColumns: Array<String>, _) = p4
+                println("--- pivot squared ")
+                p4 = p4.pivot(lhs = intArrayOf(), axis = intArrayOf(0), fanOut = *compoundColumns.indices.drop(1).toIntArray())
+                println(p4.first.contentDeepToString())
+                p4.second.first.collect { println(it.contentDeepToString()) }
+            }
+            x()
         }
     }
 }
