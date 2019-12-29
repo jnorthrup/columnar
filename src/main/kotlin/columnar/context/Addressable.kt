2da253a80b149dc8f64e855fb61d271f9aed0d6a
Jim Northrup <jim@vsiwest.com>
2019-12-29 12:25:36 +0700

    liberated the sealed

--- src/main/kotlin/columnar/context/Addressable.kt
+++ src/main/kotlin/columnar/context/Addressable.kt
@@ -3,18 +3,19 @@ package columnar.context
 import kotlin.coroutines.CoroutineContext
 
 sealed class Addressable : CoroutineContext.Element {
-    abstract class Forward<T> : Iterable<T>, Addressable()
 
-    class Indexable(
-        /**count of records*/
-        val size: () -> Int, val seek: (Int) -> Unit
-    ) : Addressable()
 
     override val key get() = addressableKey
-
-    class Abstract<T, Q>(val size: () -> Q, val seek: (T) -> Unit) : Addressable()
     companion object {
         val addressableKey = object :
             CoroutineContext.Key<Addressable> {}
     }
 }
+abstract class Forward<T> : Iterable<T>, Addressable()
+
+class Indexable(
+    /**count of records*/
+    val size: () -> Int, val seek: (Int) -> Unit
+) : Addressable()
+
+class Abstract<T, Q>(val size: () -> Q, val seek: (T) -> Unit) : Addressable()
--- src/main/kotlin/columnar/context/Ordering.kt
+++ src/main/kotlin/columnar/context/Ordering.kt
@@ -1,12 +1,6 @@
 package columnar.context
 
-import columnar.*
-import columnar.context.Medium.*
-import columnar.context.Medium.Companion.mediumKey
-import columnar.context.Medium.NioMMap.Companion.CellDriver
-import java.nio.ByteBuffer
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
 
 /**
  * ordering arranges the row and column IO chunking to tailor the io access patterns.
@@ -20,38 +14,37 @@ import kotlin.coroutines.coroutineContext
  */
 sealed class Ordering : CoroutineContext.Element {
 
-     /**
-     * [x++,y,z]
-     * [x,y++,z]
-     */
-    class RowMajor : Ordering()
-
-
-    /**
-     * [x,y++]
-     * [x++,y]
-     */
-      class ColumnMajor : Ordering()
-
-    /**
-     * {x,y,z}+-(1|n|n^?)]
-     */
-      class Hilbert : Ordering()
-
-    /**
-     * [0,0,0]
-     * [1,..1,..1]
-     * [2,..2,..2]
-     */
-      class Diagonal : Ordering()
-
-    /**for seh*/
-    abstract class RTree : Ordering()
-
     override val key: CoroutineContext.Key<Ordering> get() = orderingKey
 
     companion object {
         val orderingKey = object : CoroutineContext.Key<Ordering> {}
     }
 }
-typealias NioCursor = Matrix<Triple<() -> Any?, (Any?) -> Unit, Triple<CellDriver<ByteBuffer, Any?>, IOMemento, Int>>>
+
+/**
+ * [x++,y,z]
+ * [x,y++,z]
+ */
+class RowMajor : Ordering()
+
+
+/**
+ * [x,y++]
+ * [x++,y]
+ */
+class ColumnMajor : Ordering()
+
+/**
+ * {x,y,z}+-(1|n|n^?)]
+ */
+class Hilbert : Ordering()
+
+/**
+ * [0,0,0]
+ * [1,..1,..1]
+ * [2,..2,..2]
+ */
+class Diagonal : Ordering()
+
+/**for seh*/
+abstract class RTree : Ordering()
