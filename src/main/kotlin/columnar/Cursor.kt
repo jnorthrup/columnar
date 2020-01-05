d41b552cc46be4a89939a91821be25bfe910ddc9
Jim Northrup <jim@vsiwest.com>
2020-01-05 07:58:45 +0700

    minMax

--- src/main/kotlin/columnar/Cursor.kt
+++ src/main/kotlin/columnar/Cursor.kt
@@ -1,11 +1,10 @@
 package columnar
 
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flowOf
 import java.time.LocalDate
 import kotlin.coroutines.CoroutineContext
 
 typealias Cursor = Vect0r<RowVec>
+
 fun daySeq(min: LocalDate, max: LocalDate): Sequence<LocalDate> {
     var cursor = min
     return sequence {
@@ -15,27 +14,31 @@ fun daySeq(min: LocalDate, max: LocalDate): Sequence<LocalDate> {
         }
     }
 }
-fun Cursor.resample(indexcol:Int)=let{
+
+fun Cursor.resample(indexcol: Int) = let {
     val curs = this[indexcol]
-    val indexValues = curs.narrow().toList().map {it.first() as LocalDate}
-    val max = indexValues.toList().max()!!
-    val min = indexValues.toList().min()!!
+    val indexValues = curs.narrow() α { it: List<Any?> -> it.first() as LocalDate }
+    val (min, max) = minMax(indexValues.toSequence())
 
     val scalars = this.scalars
     val rowVecSize = scalars.size
 
 
     val indexVec = (daySeq(min, max) - indexValues).toVect0r()
-    val cursor = Cursor (  indexVec.first   ){ iy: Int ->
+    val cursor = Cursor(indexVec.first) { iy: Int ->
         RowVec(rowVecSize) { ix: Int ->
             val any = when (ix == indexcol) {
                 true -> indexVec[iy]
-                else -> null as Any?
+                else -> null
             }
-            any t2 (scalars[ix]as CoroutineContext).`⟲`
+            any t2 (scalars[ix] as CoroutineContext).`⟲`
         }
-    }as Cursor
-    combine(this as Cursor, cursor  as Cursor ) as  Cursor
+    }
+    combine(this, cursor)
 
 
 }
+
+ fun minMax(seq:Sequence<LocalDate>)= seq.fold(LocalDate.MAX t2 LocalDate.MIN) {(a,b), localDate ->
+     minOf(a, localDate) t2 maxOf (b, localDate)
+    }
