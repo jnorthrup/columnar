40fefe88037a0999d1baabf04cbc1cbf5a7ba1b2
Jim Northrup <jim@vsiwest.com>
2020-01-05 20:29:39 +0700

    fix resample

--- src/main/kotlin/columnar/Cursor.kt
+++ src/main/kotlin/columnar/Cursor.kt
@@ -17,15 +17,18 @@ fun daySeq(min: LocalDate, max: LocalDate): Sequence<LocalDate> {
 
 fun Cursor.resample(indexcol: Int) = let {
     val curs = this[indexcol]
-    val indexValues = curs.narrow() α { it: List<Any?> -> it.first() as LocalDate }
-    val (min, max) = minMax(indexValues.toSequence())
+    val indexValues = curs.narrow().map { it: List<Any?> -> it.first() as LocalDate }.toSequence()
+
+
+    val (min, max) = feature_range(indexValues)
 
     val scalars = this.scalars
     val rowVecSize = scalars.size
 
 
-    val indexVec = (daySeq(min, max) - indexValues).toVect0r()
-    val cursor = Cursor(indexVec.first) { iy: Int ->
+    val sequence = daySeq(min, max) - indexValues
+    val indexVec = sequence.toVect0r()
+    val cursor: Cursor = Cursor(indexVec.first) { iy: Int ->
         RowVec(rowVecSize) { ix: Int ->
             val any = when (ix == indexcol) {
                 true -> indexVec[iy]
@@ -37,6 +40,6 @@ fun Cursor.resample(indexcol: Int) = let {
     combine(this, cursor)
 }
 
-fun minMax(seq: Sequence<LocalDate>) = seq.fold(LocalDate.MAX t2 LocalDate.MIN) { (a, b), localDate ->
+fun feature_range(seq: Sequence<LocalDate>) = seq.fold(LocalDate.MAX t2 LocalDate.MIN) { (a, b), localDate ->
     minOf(a, localDate) t2 maxOf(b, localDate)
 }
