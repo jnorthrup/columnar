027d9a18c6fed262c6f65d6097ff51ed89223970
Jim Northrup <jim@vsiwest.com>
2020-01-06 19:23:38 +0700

    pivot

--- src/test/kotlin/columnar/CursorKtTest.kt
+++ src/test/kotlin/columnar/CursorKtTest.kt
@@ -4,8 +4,8 @@ import columnar.IOMemento.*
 import columnar.context.Columnar
 import columnar.context.NioMMap
 import columnar.context.RowMajor
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
-import kotlin.coroutines.CoroutineContext
 
 class CursorKtTest : StringSpec() {
     val coords = vZipWithNext(
@@ -35,7 +35,32 @@ class CursorKtTest : StringSpec() {
             val toList = cursor.resample(0).narrow().toList()
             toList.forEach { System.err.println(it) }
         }
-        "pivot" {
+
+        "whichKey"{
+            val fanOut_size = 2
+            val lhs_size = 2
+
+
+            fun whichKey(ix: Int) = (ix - lhs_size) / fanOut_size
+            whichKey(702) shouldBe 350
+            whichKey(700) shouldBe 349
+        }
+        "whichValue" {
+
+            val fanOut_size = 2
+            val lhs_size = 2
+
+            fun whichValue(ix: Int) = (ix - lhs_size) % fanOut_size
+            whichValue(3) shouldBe 1
+            whichValue(33) shouldBe 1
+
+            whichValue(3) shouldBe 1
+            whichValue(4) shouldBe 0
+            whichValue(0) shouldBe 0
+        }
+
+        "pivot"
+        {
             val cursor: Cursor = cursorOf(root)
             System.err.println(cursor.narrow().toList())
 //            cursor.pivot(intArrayOf(0), intArrayOf(1), intArrayOf(2, 3))
