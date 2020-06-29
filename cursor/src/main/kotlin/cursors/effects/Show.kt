30595385fc3198fa8177593bb15c86ca1f6d906e
Jim Northrup <jim@vsiwest.com>
2020-06-30 00:29:43 +0700

    for 580 million rows and change, we are somehow extrauding to 60 million and change.

--- cursor/src/main/kotlin/cursors/effects/Show.kt
+++ cursor/src/main/kotlin/cursors/effects/Show.kt
@@ -7,6 +7,7 @@ import cursors.io.left
 import cursors.io.right
 import vec.macros.size
 import vec.macros.toList
+import vec.util.debug
 import kotlin.math.min
 import kotlin.random.Random
 
@@ -17,12 +18,17 @@ fun Cursor.show(range: IntProgression = 0 until size) {
 }
 
 fun Cursor.showValues(range: IntProgression) {
-    (range).forEach {
-        println((this at it).left.toList())
+    try {
+        (range).forEach {
+            println((this at it).left.toList())
+        }
+    } catch (e: NoSuchElementException ) {
+        e  debug   "cannot fully access range $range"
     }
 }
 
 fun Cursor.head(last: Int = 5) = show(0 until (min(last, size)))
 
 
-fun Cursor.headRandom(n: Int = 5)  { head(0);showValues(Random.nextInt(size).let { it..it }) }
+fun Cursor.headRandom(n: Int = 5)  { head(0);repeat(n) { showValues(Random.nextInt(size).let { it..it }) }
+}
