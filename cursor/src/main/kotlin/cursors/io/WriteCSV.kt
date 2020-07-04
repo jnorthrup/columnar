05bc8685d9f3ec5c7382b294792641e1e0285cc5
Jim Northrup <jim@vsiwest.com>
2020-07-04 18:12:02 +0700

    csvwriter benchmark --written 317810 rows in 166s (1914.5181)/s

--- cursor/src/main/kotlin/cursors/io/WriteCSV.kt
+++ cursor/src/main/kotlin/cursors/io/WriteCSV.kt
@@ -3,13 +3,19 @@ package cursors.io
 import cursors.Cursor
 import vec.macros.size
 import vec.macros.toList
+import vec.util.fib
+import vec.util.logDebug
 import vec.util.path
 import java.nio.file.Files
 
 fun Cursor.writeCSV(fn: String) {
+
+    var trigger = 0
+    var countdown = 1
     val eol = '\n'.toInt()
     val sep = ','.toInt()
     Files.newOutputStream(fn.path).bufferedWriter().use { fileWriter ->
+        var begin = System.currentTimeMillis()
         val xsize = colIdx.size
         fileWriter.appendLine(colIdx.right.toList().joinToString(","))
         for (iy in 0 until size) {
@@ -18,7 +24,15 @@ fun Cursor.writeCSV(fn: String) {
                 if (0 < ix) fileWriter.write(sep)
                 fileWriter.write("${cell(ix)}")
             }
-            fileWriter.write(eol)
+            fileWriter.write(eol).also {
+                if (--countdown == 0) {
+                    logDebug {
+                        //without -ea this benchmark only costs a unused variable decrement.
+                        countdown = fib(++trigger)
+                        val l = (System.currentTimeMillis() - begin) / 1000
+                        "written $iy rows in ${l}s (${iy.toFloat()/l.toFloat()})/s" }
+                }
+            }
         }
     }
 }
