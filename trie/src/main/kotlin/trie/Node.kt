980dfef5447f6121967237be5b4bc40f500f12c0
Jim Northrup <jim@vsiwest.com>
2020-07-08 16:47:50 +0700

    narrowed down trie to something a little more stupid

--- trie/src/main/kotlin/trie/Node.kt
+++ trie/src/main/kotlin/trie/Node.kt
@@ -6,9 +6,4 @@ import java.util.concurrent.atomic.AtomicBoolean
 /**
  * Created by kenny on 6/6/16.
  */
-//data class Node<T, V>(val value: T?,
-//                      var isLeaf: Boolean,
-//                      val payload: V,
-//                      val children: MutableMap<T,Node<T,V>> = mutableMapOf()
-//)
 typealias Node<T > = Qu4d< T  , AtomicBoolean,Int,MutableMap<T,*>>
