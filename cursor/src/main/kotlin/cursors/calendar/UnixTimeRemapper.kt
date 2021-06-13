777858cd81a2d77fed8bf8b3c64dce11bbb2abc2
jim <jim@vsiwest.com>
2021-06-13 22:14:07 +0800

    stylistic tweaks

--- cursor/src/main/kotlin/cursors/calendar/UnixTimeRemapper.kt
+++ cursor/src/main/kotlin/cursors/calendar/UnixTimeRemapper.kt
@@ -1,17 +1,18 @@
 package cursors.calendar
 
+//import cursors.io.left
 import cursors.Cursor
-import cursors.at
+import cursors.context.Arity
 import cursors.context.Scalar
 import cursors.get
-import cursors.io.IOMemento
+import cursors.io.IOMemento.IoInstant
 import cursors.io.RowVec
-import cursors.io.Vect02_.Companion.left
 import cursors.io.colIdx
-//import cursors.io.left
 import cursors.macros.join
 import cursors.unaryMinus
-import vec.macros.*
+import vec.macros.size
+import vec.macros.t2
+import vec.macros.α
 import java.time.Instant
 
 class UnixTimeRemapper {
@@ -24,35 +25,17 @@ class UnixTimeRemapper {
         @JvmStatic
         fun timestampFromIoLong(vararg timestampColumnNames: String): (Cursor) -> Cursor = { c0: Cursor ->
             val newKeys: IntArray = c0.colIdx.get(*timestampColumnNames)
-            val s = timestampColumnNames.map(String::unaryMinus).toTypedArray()
-            val thinned = c0.colIdx.get(*s)
+            val thinned: IntArray = c0.colIdx.get(*timestampColumnNames.map(String::unaryMinus).toTypedArray()) //-"column"
             val leftovers: Cursor = c0[thinned]
-
-            val c2 :Cursor =
-                Cursor(c0.size) { y: Int ->
-                    val rv1=c0 at  y
-                    val vals  = rv1.left
-
-                    RowVec(newKeys.size) { x: Int ->
-                        val i = newKeys[x]
-                        val any = vals[i]
-                        val v2  = (any as? Long) ?: any.toString().toLong()
-                        Instant.ofEpochMilli(v2) t2 { Scalar(IOMemento.IoInstant, timestampColumnNames[x]) }
-                    }
-                }
-
-
-            /*
-            c0[newKeys] α { (a, b) ->
-                a t2 b α { (valueStringOrLong, bbContext) ->
-                    Instant.ofEpochMilli((valueStringOrLong as? Long) ?: valueStringOrLong.toString().toLong()) t2 {
-                        val (_, f) = bbContext().get(Arity.arityKey) as Scalar;
-                        Scalar(IOMemento.IoInstant, f)
+            val c2: Cursor = c0[newKeys] α { rv: RowVec ->
+                rv.size t2 rv.second α { (unixtime, desc) ->
+                    Instant.ofEpochMilli((unixtime as? Long) ?: unixtime.toString().toLong()) t2 {
+                        val (_, f) = desc().get(Arity.arityKey) as Scalar
+                        Scalar(IoInstant, f)
                     }
                 }
             }
-*/
-            join( c2, leftovers)
+            join(c2, leftovers)
         }
     }
 }
