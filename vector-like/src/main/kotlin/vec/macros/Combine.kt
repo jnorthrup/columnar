d9164c5ba2262661276e7477b0cf9cd8b6214142
jim <jim@vsiwest.com>
2021-06-19 02:32:25 +0800

    certain features related to Cursor.scalars fail when there are 0 rows.  there can be no blackboard context for no cells.

--- vector-like/src/main/kotlin/vec/macros/Combine.kt
+++ vector-like/src/main/kotlin/vec/macros/Combine.kt
@@ -6,17 +6,20 @@ import kotlin.math.absoluteValue
 inline fun <reified T> combine(vargs: Vect0r<Vect0r<T>>) = combine(*vargs.toArray())
 
 @JvmName("combine_VecVa")
-inline fun <reified T> combine(vararg vargs: Vect0r<T>): Vect0r<T> =
-    muxIndexes(vargs.size t2 { it: Int -> vargs[it] }).let { (isize, tails) ->
-        isize t2 { ix: Int ->
-            val (slot, i1) = demuxIndex(tails, ix)
-            vargs[slot][i1]
+inline fun <reified T> combine(vararg vargs: Vect0r<T>): Vect0r<T> {
+    vargs.filter { it.size>0 }.toVect0r().let {vargs1->
+        return muxIndexes(vargs1).let { (isize, tails) ->
+            isize t2 { ix: Int ->
+                val (slot, i1) = demuxIndex(tails, ix)
+                vargs1[slot][i1]
+            }
         }
     }
+}
 
 inline fun <reified T> muxIndexes(vargs: Vect0r<Vect0r<T>>) =
-    vargs.toArray()
-        .foldIndexed(0 t2 IntArray(vargs.size)) { vix: Int, (acc: Int, srcVec: IntArray): Pai2<Int, IntArray>, (vecSize): Vect0r<T> ->
+    vargs.toArray()/*.filter { it.size>0 }*/.foldIndexed(0 t2 IntArray(vargs.size)) {
+            vix: Int, (acc: Int, srcVec: IntArray): Pai2<Int, IntArray>, (vecSize): Vect0r<T> ->
             (acc + (vecSize)).let { nsize ->
                 srcVec[vix] = nsize
                 nsize t2 srcVec
