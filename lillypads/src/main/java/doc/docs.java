88965d65825b17d6469b1abf62fbf373a71b6085
jim <jim@vsiwest.com>
2021-10-22 23:30:13 +0800

    mayhem commit enabling toNumber and eliminating inline Number classes when gk NUID addressSpaces don't cross the sign bit.

--- lillypads/src/main/java/doc/docs.java
+++ lillypads/src/main/java/doc/docs.java
@@ -1,9 +1,10 @@
 package doc;
 
+/***
+ * heres a doc class.  this serves as a shim for maven to not error out of creating a doc jarfile.  sorry that's all we kotliners have atm.
+ *
+ */
 public class docs {
     public void waitingForKotlinJavadocs() {
-        ;
     }
-
-    ;
 };
