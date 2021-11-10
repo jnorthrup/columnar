0be7ec630a6cd62306867992a5f3912e9035db2b
jim <jim@vsiwest.com>
2021-11-10 17:33:14 +0800

    cleaned out hello.kt except the very basic examples.
    Former-commit-id: 83a0768db28dcfee123bb720ffd161c3e17b648e

--- columnar/c_async/src/linuxX64Main/kotlin/simple/main.kt
+++ columnar/c_async/src/linuxX64Main/kotlin/simple/main.kt
@@ -24,56 +24,6 @@ fun open_read() {
 }
 
 
-fun epoll_popen_loop_cat(): Int = memScoped {
-
-    val pIoFile: CPointer<FILE> =
-        popen("cat /etc/sysctl.conf </dev/null", "r")!!   //works when stdin is piped, not dev/null tho
-
-    val event: epoll_event = alloc()
-    event.data.fd = pIoFile.pointed._fileno
-    event.events = (EPOLLIN or EPOLLPRI or EPOLLERR).toUInt()
-
-    val epoll_fd:Int = epoll_create1(0)
-    val epollCtl =
-        epoll_ctl(epoll_fd, EPOLL_CTL_ADD as Int, event.data.fd as Int, event.ptr as CValuesRef<epoll_event>)
-    if (0!=epollCtl) {
-        fprintf(stderr, "Failed to add file descriptor to epoll_popen_loop_cat\n")
-        close(epoll_fd)
-        return 1
-    }
-
-    val  READ_SIZE : Int = 10
-    var running:Int = 1
-    while (0!=running) {
-        printf("\nPolling for input...\n")
-    val   MAX_EVENTS : Int = 5
-        val events:CArrayPointer<epoll_event> = allocArray(5)
-        val event_count:Int = epoll_wait(epoll_fd, events, MAX_EVENTS, 3000)
-
-        printf("%d ready events\n", event_count)
-        for (i in   0 until event_count) {
-            val fd:Int = events[i].data.fd
-            printf("Reading file descriptor '%d' -- ", fd)
-
-            val  buf:CArrayPointer<ByteVar> = allocArray( READ_SIZE + 1)
-            val bytes_read:ssize_t = read(fd, buf, READ_SIZE.toULong())
-            printf("%zd bytes read.\n", bytes_read)
-            if (bytes_read < 1) {
-                running = 0
-                break
-            }
-            buf[bytes_read.toInt()] = 0.toByte()
-            printf("Read '%s'\n", buf)
-
-        }
-    }
-
-    if (0!=close(epoll_fd)) {
-        fprintf(stderr, "Failed to close epoll_popen_loop_cat file descriptor\n")
-        return 1
-    }
-    return 0
-}
 
 fun open_mmap(): Unit {
     memScoped {
@@ -93,7 +43,6 @@ fun main() {
         open_read()
         cFileRead()
         open_mmap()
-        epoll_popen_loop_cat()
     } catch (e: kotlin.IllegalArgumentException) {
         e.printStackTrace()
     }
