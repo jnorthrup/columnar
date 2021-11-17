a173045e341282f4ecccec481f4ae26733baf35f
jim <jim@vsiwest.com>
2021-11-18 01:42:49 +0800

    echo server
    Former-commit-id: 38e82b57e22cf0f237dde6bf628166af6438269b

---
+++ columnar/echo_uring/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("multiplatform") version "1.6.0"
+}
+kotlin {
+    linuxX64 {
+        binaries {
+            executable("uring_echo"//, listOf(DEBUG, RELEASE)
+            ) {
+                // Base name for the output file.
+                baseName = "uring_echo"
+                // Accessing the output file.
+                println("Executable path: ${outputFile.absolutePath}")
+                // Custom entry point function.
+                entryPoint = "lib_uring.echo"
+            }
+        }
+        val main by compilations.getting {
+            compilations["main"].cinterops {
+                create("native") {
+                    defFile = project.file("src/nativeInterop/cinterop/uring_echo.def")
+                }
+            }
+        }
+    }
+
+    sourceSets {
+
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":cat_uring"))
+            }
+        }
+        val linuxX64Main by getting {
+            dependsOn(commonMain)
+        }
+
+    }
+}
