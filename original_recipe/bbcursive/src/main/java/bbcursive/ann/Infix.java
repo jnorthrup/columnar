a6252f9b7d82d211f275250595b51f34ac0f7248
jim <jim@vsiwest.com>
2021-11-20 04:55:05 +0800

    bbcursive
    Former-commit-id: 449bc58fa854a2e9e534a76063dc1af2da269e3f

---
+++ original_recipe/bbcursive/src/main/java/bbcursive/ann/Infix.java
@@ -0,0 +1,13 @@
+package bbcursive.ann;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *negates skipper
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Infix
+{
+
+}
