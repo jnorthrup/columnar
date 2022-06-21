47e0dc40c39e82a22439e0a72b7f5934c84b724f
jim <northrup.james@gmail.com>
2022-06-22 00:26:55 +0800

    ta4j behaving with horizon-size max candles.
    Former-commit-id: 382e3404d33ebe5ed43f2145a434a402c3886b82

--- original_recipe/vector-like/src/main/java/vec/util/FibonacciReporter.kt
+++ original_recipe/vector-like/src/main/java/vec/util/FibonacciReporter.kt
@@ -3,28 +3,32 @@
 package vec.util
 
 import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import kotlin.math.max
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 
+@OptIn(ExperimentalTime::class)
+
 class FibonacciReporter(val size: Int? = null, val noun: String = "rows") {
     var trigger = 0
     var countdown = 1
     val begin = TimeSource.Monotonic.markNow()
-    @OptIn(ExperimentalTime::class)
-    fun report(iy: Int) =
-        if (--countdown == 0) {
-            //without -ea this benchmark only costs a unused variable decrement.
-            countdown = fib(++trigger)
-            val l = begin.elapsedNow()
-            val slice = l / max(1, iy)
 
-            "logged $iy $noun in $l " + (size?.let {
-                val ticksLeft = size - iy
-                val remaining: Duration = slice* ticksLeft
-                val inWholeSeconds = l.inWholeSeconds
-                "${  iy.toFloat() /inWholeSeconds.toFloat()}/s remaining: $remaining est ${Clock.System.now().plus(remaining)}"
-            } ?: "")
-        } else null
+    fun report(iy: Int) =
+            if (--countdown == 0) {
+                //without -ea this benchmark only costs a unused variable decrement.
+                countdown = fib(++trigger)
+                val l = begin.elapsedNow()
+                val slice = l / max(1, iy)
+                val secondsSinceBegin = l.inWholeSeconds
+
+                "logged $iy $noun in $l ${(iy.toDouble() / (secondsSinceBegin.toDouble())).toFloat()}/s " + (size?.let {
+                    val ticksLeft = size - iy
+                    val remaining: Duration = slice * ticksLeft
+                    "remaining: $remaining est ${Clock.System.now().plus(remaining).toLocalDateTime(TimeZone.currentSystemDefault())}"
+                } ?: "")
+            } else null
 }
